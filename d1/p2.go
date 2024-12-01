eb4a0074d21590e4814a2610d9184286fe78072e
Edward Potter <74555065+TA-Solaris@users.noreply.github.com>
2024-12-01 19:45:15 +0000

    Consistent tab sizing

--- d1/p2.go
+++ d1/p2.go
@@ -3,53 +3,53 @@ package main
 import (
   "fmt"
   "io/ioutil"
-	"strings"
-	"strconv"
+  "strings"
+  "strconv"
 )
 
 type Input struct {
-	List1 []int
-	List2 []int
+  List1 []int
+  List2 []int
 }
 
 func parseInput(strInput string) Input {
-	lines := strings.Split(strings.TrimSpace(strInput), "\n")
+  lines := strings.Split(strings.TrimSpace(strInput), "\n")
 
-	input := Input{
-		List1: make([]int, len(lines)),
-		List2: make([]int, len(lines)),
-	}
+  input := Input{
+    List1: make([]int, len(lines)),
+    List2: make([]int, len(lines)),
+  }
 
-	for i, line := range lines {
-		parts := strings.Split(line, "   ")
-		input.List1[i], _ = strconv.Atoi(parts[0])
-		input.List2[i], _ = strconv.Atoi(parts[1])
-	}
+  for i, line := range lines {
+    parts := strings.Split(line, "   ")
+    input.List1[i], _ = strconv.Atoi(parts[0])
+    input.List2[i], _ = strconv.Atoi(parts[1])
+  }
 
-	return input
+  return input
 }
 
 func calculate(input Input) int {
 
-	counts := countOccurrences(input.List2)
+  counts := countOccurrences(input.List2)
 
-	total := 0
+  total := 0
 
-	for _, num := range input.List1 {
-		if occurrences, ok := counts[num]; ok {
-			total += num * occurrences
-		}
-	}
+  for _, num := range input.List1 {
+    if occurrences, ok := counts[num]; ok {
+      total += num * occurrences
+    }
+  }
 
-	return total
+  return total
 }
 
 func countOccurrences(nums []int) map[int]int {
-	counts := make(map[int]int)
-	for _, num := range nums {
-		counts[num]++
-	}
-	return counts
+  counts := make(map[int]int)
+  for _, num := range nums {
+    counts[num]++
+  }
+  return counts
 }
 
 func main() {
@@ -58,8 +58,8 @@ func main() {
     panic(err)
   }
 
-	input := parseInput(string(data))
-	output := calculate(input)
+  input := parseInput(string(data))
+  output := calculate(input)
 
   fmt.Println(output)
 }
